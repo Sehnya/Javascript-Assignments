4b7d931342a4275ca734e032a1a7b606fb765295
Seh_nya <sehnyaw@gmail.com>
2024-12-18 04:33:01 -0500

    removed code from .js; coded js into html file

--- HTML/new-file.js
+++ HTML/new-file.js
@@ -1,25 +1,2 @@
-
-function add7() {
-    return (Number) + 7;
-};
-
-function multiply(a,b) {
-    return a * b;
-};
-
-function capitalize() {
-    return "?".charAt(0).toUpperCase() + str.slice(1);
-};
-
-function lastLetter () {
-    return "?".slice(-1);
-}
-
 const a = "hello!";
 const b = 6;
-
-console.log(capitalize(a));
-console.log(multiply(b));
-console.log(add7(b));
-console.log(lastLetter(a));
-
