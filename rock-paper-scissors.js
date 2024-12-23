b48ba07544073b376cedc5fdadc17466312e35c0
Seh_nya <sehnyaw@gmail.com>
2024-12-23 01:32:10 -0500

    loop implementation with try again button

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,4 @@
-
-// let round = 1;
-
 document.addEventListener("DOMContentLoaded", () => {
-    globalThis.humanScore = 0;
-    globalThis.computerScore = 0;
     const roundDisplay = document.getElementById('round');
     const buttons = document.querySelectorAll("#RPScontainer button");
     const choices = Array.from(buttons).map(button => button.id);
@@ -11,43 +6,84 @@ document.addEventListener("DOMContentLoaded", () => {
     const computerChoiceDisplay = document.getElementById('computer-choice');
     const winnerDisplay = document.getElementById('winner');
     const scoreDisplay = document.getElementById('score');
-
+    const gameWinnerDisplay = document.getElementById('game');  
+    
+    let round = 0
+    let humanScore = 0
+    let computerScore = 0;
+ 
     function getComputerChoice() {
+        
         return choices[Math.floor(Math.random() * buttons.length)];
     }
+
     function determineW(compChoice,userChoice) {
         if (compChoice === userChoice) {
-            return ("It's a TIE!! No Points Awarded.")
+
+        return ("It's a TIE!! No Points Awarded.")
+
         } else if (
-            (compChoice === 'paper' && userChoice == 'rock') ||
-            (compChoice === 'scissors' && userChoice === 'paper') ||
-            (compChoice === 'rock' && userChoice === 'scissors')) {
-                computerScore++;
-            return (`The winner is computer with ${compChoice}`);
+
+        (compChoice === 'paper' && userChoice == 'rock') ||
+        (compChoice === 'scissors' && userChoice === 'paper') ||
+        (compChoice === 'rock' && userChoice === 'scissors')) {
             
+            computerScore++;
+            return (`The winner is computer with ${compChoice}`);
+
         } else {
-                humanScore++;
-            return (`The winner is the user with ${userChoice}`);
+
+            humanScore++;
+
+        return (`The winner is the user with ${userChoice}`);
         };
     };
-    
+      
     buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        const compChoice = getComputerChoice();
-        const userChoice = button.id
-        const winner = determineW(userChoice,compChoice);
-        let round = 0; 
-        round++;
-        
-        roundDisplay.textContent = `Round: ${round}`
-        userChoiceDisplay.textContent = `User choice: ${userChoice}`;
-        computerChoiceDisplay.textContent = `Computer choice: ${compChoice}`;
-        scoreDisplay.textContent = `User: ${humanScore}\nComputer:${computerScore}\n`;
-        winnerDisplay.textContent = winner
-         });
-     });
-});
 
+        button.addEventListener("click", () => {
+
+            if (round < 5) {
+                const compChoice = getComputerChoice();
+                const userChoice = button.id
+                const winner = determineW(userChoice,compChoice);
+          
+                round++;
+                roundDisplay.textContent = `Round: ${round} out of 5`
+                userChoiceDisplay.textContent = `User choice: ${userChoice}`;
+                computerChoiceDisplay.textContent = `Computer choice: ${compChoice}`;
+                scoreDisplay.textContent = `User: ${humanScore}\nComputer:${computerScore}\n`;
+                winnerDisplay.textContent = winner;
+             
+                if (round === 5) {
+
+                    if (humanScore > computerScore) {
+
+                    gameWinnerDisplay.textContent = `You won the game! Congrats!`;
+                    buttons.forEach(button => button.disabled = true);
+
+                    } else if (computerScore > humanScore) {
+
+                    gameWinnerDisplay.textContent = 'WOMP! WOMP! The computer won the game! Better luck next time!';
+                    buttons.forEach(button => button.disabled = true);
+
+                    } else {
+
+                    gameWinnerDisplay.textContent = `Neither player reached a score of 3. It's a DRAW!`;
+
+                    }
+                    buttons.forEach(button => button.disabled = true);
+
+                    document.getElementById('try-again').removeAttribute('hidden')
+                    document.getElementById('try-again').addEventListener ('click', () => {
+                        location.reload(true);
+                    });
+                }
+            }
+        });
+    });    
+});
+  
 
 //     let max = 3;
 //     let compChoice = buttons[Math.floor(Math.random(max) * choices.length)];
@@ -82,4 +118,3 @@ document.addEventListener("DOMContentLoaded", () => {
     //     }
     //     console.log(`SCORE:\n You: ${this.humanScore},\n Computer: ${this.computerScore}`);
 //  };
-//
